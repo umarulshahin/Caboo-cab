5cd2594a6701647d903fae26a098c3bb44f80f58
umarulshahin <akkushahin666@gmail.com>
2024-10-05 10:22:14 +0530

    deployment bug fix 2

--- Backend/Caboo_backend/User_app/views.py
+++ Backend/Caboo_backend/User_app/views.py
@@ -176,6 +176,7 @@ def Walletdetails(request):
 @permission_classes([IsAuthenticated])
 def Review(request):
     try:
+        print('pani yaan idh 1')
         data = request.data
         trip = TripDetails.objects.get(id=data['tripId'])
         serializer =TripSerializer(trip,data,partial=True)
